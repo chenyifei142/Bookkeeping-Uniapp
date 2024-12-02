50866986afe6c007d824b60e533cd53838515d78
chenyf <chenyifei142@gmail.com>
2024-12-02 17:37:51 +0800

    chore:add uview-plus

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import '@/styles/index.scss'
+import uviewPlus from 'uview-plus'
 
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(uviewPlus)
   return {
-    app,
+    app
   };
 }
